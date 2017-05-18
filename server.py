8e4b4982455d35a8a2de94fea04a062b52a9f685
Arik Hadas <ahadas@redhat.com>
2017-05-18 14:35:05 +0300

    basic server
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

---
+++ server.py
@@ -0,0 +1,45 @@
+import time
+import BaseHTTPServer
+import json
+
+HOST_NAME = '10.35.1.90' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 9000 # Maybe set this to 9000.
+
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html><head><title>Title goes here.</title></head>")
+        s.wfile.write("<body><p>This is a test.</p>")
+        # If someone went to "http://something.somewhere.net/foo/bar/",
+        # then s.path equals "/foo/bar/".
+        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+        s.wfile.write("</body></html>")
+    def do_POST(s):
+        print 'got something'
+        content_len = int(s.headers.getheader('content-length', 0))
+        post_body = s.rfile.read(content_len)
+#        print post_body
+        stats = json.loads(post_body)
+        print stats
+        for stat in stats:
+            if (stat['plugin'] == 'virt'):
+                print stat
+ 
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
